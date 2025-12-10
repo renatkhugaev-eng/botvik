c74d624ae4d6b5f28f8177ee751af47e9b125137
renatkhugaev-eng <renatkhugaev-eng@users.noreply.github.com>
2025-12-11 00:44:55 +0400

    Use edge Prisma client with Neon adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 const globalForPrisma = globalThis as unknown as {
